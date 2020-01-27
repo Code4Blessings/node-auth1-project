6538ce06ba482f9fe5d6eda8fd75497bf077c9da
Code4Blessings <robinwarden@code4blessings.com>
2020-01-27 18:03:41 -0500

    Updated user-router and auth-router files

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,3 +3,22 @@ const router = require('express').Router();
 
 const Users = require('../users/user-model');
 
+router.post('/register', (req, res) => {
+    const userData = req.body;
+
+        users.insert(userData)
+        .then(id => {
+            res.status(201).json({created: id})
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                    errorMessage: "User account could not be created",
+                    message: err.message
+             });
+            });
+        })
+
+
+
+module.exports = router;
--- users/user-router.js
+++ users/user-router.js
@@ -4,4 +4,19 @@ const dB = require('../data/dbConfig');
 
 const Users = require('./user-model');
 
+router.get('/', (req, res) => {
+ Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ 
+            errorMessage: "Error retrieving the list of users", 
+            message: err.message
+        })
+    })
+})
+
+
 module.exports = router;
