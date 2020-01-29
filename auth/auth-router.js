8b35ac183f9c63b5af1e50935faa7d4e268cb49e
Code4Blessings <robinwarden@code4blessings.com>
2020-01-29 16:08:05 -0500

    Updated restricted-middleware and auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -42,6 +42,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bc.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.userId = user.id;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -4,6 +4,10 @@ const Users = require('../users/user-model');
 
 module.exports = (req, res, next) => {
     //use session information to restrict access
-    console.log(req.session)
-    next()
+    console.log(req.session);
+    if(req.sessions && req.session.loggedIn){
+    next();
+    } else {
+        res.status(401).json({you: 'You shall not pass!'})
+    }
 }
